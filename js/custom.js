41739ee5137b79c1a02122185437c8f9242d5968
ElsieFelbah <64813193+ElsieFelbah@users.noreply.github.com>
2020-11-26 22:19:34 +0000

    b

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,16 @@
      //animate on scroll
      new WOW().init();   
  });
+
+ /*================================================
+                   WORK
+==================================================*/
+$(function(){
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{
+            enabled:true
+          }
+    });
+});
